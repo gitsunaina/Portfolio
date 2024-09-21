f2d37d538e0f67af4dddd5acca9a5aaa84517285
sunaina26 <138777512+gitsunaina@users.noreply.github.com>
2024-09-21 16:27:43 +0530

    make responsive continue

--- src/pages/Components/About.jsx
+++ src/pages/Components/About.jsx
@@ -5,13 +5,13 @@ const About = () => {
     //     'javascript', 'reactjs', 'redux', 'next js', 'html', 'css', 'bootstrap', 'tailwind', 'git', 'github','react admin', 'adobe photoshop' , 'canva'
     // ]
   return (
-    <section className='lg:max-w-[1290px] lg:mx-auto my-[11rem]' id='about'>
+    <section className='w-full lg:max-w-[1290px] lg:mx-auto  my-[11rem] about-container' id='about'>
           
                 <h2 className='font-bold text-[50px] font-serif text-[#eaebec]'>About</h2>
                 <div className='flex gap-[10px] mt-[20px]'>
                 <div className='w-[110px] h-[3px] bg-[#ffc86b] mt-[15px]'></div>
-               <div className='flex flex-col gap-[10px]'>
-               <p className='text-[#eaebec] text-[22px]'>Experienced Javascript Developer with <span className='text-[#ffc86b]'> 1 years </span> in the industry. Proficient with <span className='text-[#ffc86b]'>React</span>. Used problem-solving aptitude to enhance application performance by <span className='text-[#ffc86b]'>15%</span>. Enhanced College based Application for Education Organisation.</p>
+               <div className='flex flex-col gap-[10px] w-full about-desc'>
+               <p className='text-[#eaebec] text-[18px] sm:text-[18px] lg:text-[22px] sm:w-full'>Experienced Javascript Developer with <span className='text-[#ffc86b]'> 1 years </span> in the industry. Proficient with <span className='text-[#ffc86b]'>React</span>. Used problem-solving aptitude to enhance application performance by <span className='text-[#ffc86b]'>15%</span>. Enhanced College based Application for Education Organisation.</p>
                         <div className='flex gap-[36px] mt-[26px]'>
                     <div className='about-batch text-white items-center flex flex-col p-[19px] rounded-[36px] max-w-[250px] w-[200px] border-[1px] shadow-[0px_0px_15px_0px]'>
                     <span className='mb-[25px]'><i class="fa-solid fa-award"></i></span>
