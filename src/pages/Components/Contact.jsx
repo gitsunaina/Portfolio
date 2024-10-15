ae09f5910f80c8cb301a7c55277d08481075c21c
sunaina26 <138777512+gitsunaina@users.noreply.github.com>
2024-10-16 00:46:44 +0530

    update files

--- src/pages/Components/Contact.jsx
+++ src/pages/Components/Contact.jsx
@@ -25,6 +25,7 @@ const Contact = () => {
             <div className='flex flex-col justify-center  gap-4 lg:p-[50px] p-0 order-1 lg:order-2'>
                 
 <form name="contact" method="POST" data-netlify="true">
+<input type="hidden" name="form-name" value="contact" />
   <label className="block mb-3 text-sm font-medium text-[#ffc86b] dark:text-white">Name</label>
   <div className="relative mb-5">
     <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
@@ -34,7 +35,7 @@ const Contact = () => {
       </svg> */}
       <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" fill="currentColor" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z"/></svg>
     </div>
-    <input type="text" id="name-icon" name='name' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your Name" />
+    <input type="text"  name='name' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your Name" />
   </div>
 
   <label className="block mb-3 text-sm font-medium text-[#ffc86b] dark:text-white">Email</label>
@@ -45,11 +46,11 @@ const Contact = () => {
         <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
       </svg>
     </div>
-    <input type="email" name='email' id="email-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your Email" />
+    <input type="email" name='email'  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your Email" />
   </div>
 
   <label className="block mb-3 text-sm font-medium text-[#ffc86b] dark:text-white">Your message</label>
-  <textarea id="message" rows="8" name='message' class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+  <textarea  rows="8" name='message' class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
   <button className="hero-btn mt-6  w-full text-white border-[#ffc86b] border-[1px] rounded-md p-[12px] shadow-[0px_0px_10px_0px]" type='submit'>Send Message</button>
 </form>
 
